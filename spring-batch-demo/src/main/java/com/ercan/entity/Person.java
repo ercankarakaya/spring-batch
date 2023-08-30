b22758e98dd10e8e8cd70bf10ebbc64b61c5e5ab
Ercan Karakaya <karakayaercan71@gmail.com>
2023-08-30 23:49:34 +0300

    Person entity and person.csv created.

---
+++ spring-batch-demo/src/main/java/com/ercan/entity/Person.java
@@ -0,0 +1,49 @@
+package com.ercan.entity;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private Date birthDate;
+    private String country;
+    private String phoneNumber;
+
+    @SneakyThrows
+    @Override
+    public String toString() {
+//        final StringBuffer sb = new StringBuffer("");
+//        sb.append("Person{");
+//        sb.append("id:").append(id);
+//        sb.append(", firstName:").append(firstName).append('\'');
+//        sb.append(", lastName:").append(lastName).append('\'');
+//        sb.append(", email:").append(email).append('\'');
+//        sb.append(", gender:").append(gender).append('\'');
+//        sb.append(", birthDate:").append(birthDate).append('\'');
+//        sb.append(", country:").append(country).append('\'');
+//        sb.append(", phoneNumber:").append(phoneNumber);
+//        sb.append('}');
+//        return sb.toString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(this);
+    }
+}
